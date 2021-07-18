d9c18a6d60db52252103e1edca0233b002342155
sheikh rehman <sheikhrehman5870@gmail.com>
2021-07-18 12:41:05 +0500

    start

---
+++ app/src/main/java/com/example/gitprojectpart2/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.gitprojectpart2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    val value1 = 10
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
