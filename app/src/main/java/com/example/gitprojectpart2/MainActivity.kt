462031e9bcf2eff9c9a108e00497fc06cb2f7f9e
sheikh rehman <sheikhrehman5870@gmail.com>
2021-07-18 12:44:20 +0500

    start-change

--- app/src/main/java/com/example/gitprojectpart2/MainActivity.kt
+++ app/src/main/java/com/example/gitprojectpart2/MainActivity.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
 
+
+    val value2 =20
     val value1 = 10
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
